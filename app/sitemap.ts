f7fdcc709cdd01297004816d7048b12087e5f462
Hatim Ahmed Hassan <hatim@beamrobotics.com>
2026-07-04 15:18:26 +0300

    fixed sitemap

--- app/sitemap.ts
+++ app/sitemap.ts
@@ -1,10 +1,12 @@
 import type { MetadataRoute } from "next";
 
+export const dynamic = "force-static";
+
 export default function sitemap(): MetadataRoute.Sitemap {
   return [
     {
       url: "https://xaatim.github.io/hatim-portfolio",
-      lastModified: new Date(),
+      lastModified: new Date().toISOString().split("T")[0],
       changeFrequency: "monthly",
       priority: 1,
     },
